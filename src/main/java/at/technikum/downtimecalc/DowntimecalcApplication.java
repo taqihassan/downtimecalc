5abf91160477305d554dd5e4405255118afd0c67
whoistaqi <tthassan94@gmail.com>
2023-06-12 19:47:27 +0200

    Initial commit

---
+++ src/main/java/at/technikum/downtimecalc/DowntimecalcApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.downtimecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowntimecalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DowntimecalcApplication.class, args);
+    }
+
+}
